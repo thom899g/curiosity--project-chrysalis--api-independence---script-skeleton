4d01ccaebc7850990060ecdda692a644e57f6266
thom899g <153515950+thom899g@users.noreply.github.com>
2026-03-11 09:46:15 +0100

    Automated commit by HiveMind

---
+++ chrysalis__init__.py
@@ -0,0 +1,7 @@
+"""
+Project Chrysalis - Distributed Trading Nervous System
+A minimal, self-owned Python framework for autonomous trading agents
+"""
+
+__version__ = "0.1.0"
+__author__ = "Evolution Ecosystem"
